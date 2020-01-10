459f5bd311b60d6c450bf4f5428500d66919e7d4
Fumiaki Kinoshita <fumiexcel@gmail.com>
2020-01-10 22:42:06 +0900

    default to HumblePrelude.Extras

--- src/HumblePrelude/Plugin.hs
+++ src/HumblePrelude/Plugin.hs
@@ -1,20 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  HumblePrelude.Plugin
+-- Copyright   :  (c) Fumiaki Kinoshita 2020
+-- License     :  BSD-style (see the file LICENSE)
+--
+-- Maintainer  :  fumiexcel@gmail.com
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- GHC plugin which adds imports to specified modules
+--
+-----------------------------------------------------------------------------
 module HumblePrelude.Plugin (plugin) where
 
 import HumblePrelude
-import Control.Exception (throwIO)
-import GhcPlugins
+import GhcPlugins hiding ((<>))
 import HsSyn (hsmodImports, simpleImportDecl)
-import Panic (GhcException(..))
 
+-- | @Usage: -fplugin=HumblePrelude.Plugin@
 plugin :: Plugin
 plugin = defaultPlugin
   { parsedResultAction = parsedPlugin
   , pluginRecompile = flagRecompile }
 
 parsedPlugin :: [CommandLineOption] -> ModSummary -> HsParsedModule -> Hsc HsParsedModule
-parsedPlugin [name] _ pm = do
-  let rep = noLoc $ simpleImportDecl $ mkModuleName name
-  pure pm { hpm_module = fmap (\m -> m { hsmodImports = rep:hsmodImports m }) $ hpm_module pm }
-parsedPlugin _ _ _ = liftIO $ throwIO
-  $ ProgramError "Please specify a module name (e.g -fplugin-opt=HumblePrelude.Plugin:HumblePrelude.Extras)"
+parsedPlugin [] ms hpm = parsedPlugin ["HumblePrelude.Extras"] ms hpm
+parsedPlugin mods _ pm = do
+  let rep = noLoc . simpleImportDecl . mkModuleName <$> mods
+  pure pm { hpm_module = fmap (\m -> m { hsmodImports = rep <> hsmodImports m }) $ hpm_module pm }
